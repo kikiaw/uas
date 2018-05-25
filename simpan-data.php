8edd4fc3a3e74a2c8fa061588c1f8df2257af364
kikiaw <pungkiapriw@gmail.com>
2018-05-25 16:13:34 +0700

    first commit

---
+++ simpan-data.php
@@ -0,0 +1,35 @@
+<h1 style="text-align:center;">Pengisian Data Mahasiswa</h1>
+<h2 style="color:black;text-align:center;">Pungky Apri Wibowo</h2>
+<hr>
+<?php
+include "koneksi.php";
+$koneksiObj = new Koneksi();
+$koneksi = $koneksiObj->getKoneksi();
+if($koneksi->connect_error) {
+    echo "Gagal koneksi : " . $koneksi->connect_error;
+} else {
+    echo "Sambungan basis data berhasil";
+}
+/*
+echo 'id : ' . $_POST["id"];
+echo "<br>";
+echo "nama : " . $_POST["nama"];
+echo "<br>";
+echo "alamat : " . $_POST["alamat"];
+echo "<br>";
+echo "no hp : " . $_POST["nohp"];
+echo "<br>";
+*/
+$query = "insert into data_member(id, nama, alamat,no_hp) " .
+        "values(" . $_POST["id"] . ",'" . 
+        $_POST["nama"] . "','" . $_POST["alamat"] . "',".$_POST["nohp"] . ")";
+        echo $query;
+//echo "<br>".$query;
+if($koneksi->query($query) === true) {
+    echo "<br> Data dengan nama '" . $_POST["nama"] . "' berhasil disimpan ke database." .
+            ' <a href="main.php">Lihat Data Anda</a>';
+} else {
+    echo "<br>Data GAGAL disimpan";
+}
+$koneksi->close();
+?>
